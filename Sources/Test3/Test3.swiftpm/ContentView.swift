4f673f8dac2ffa1e5c40e6fd62ff04b06d759ab5
bryandebourbon <bryandebourbon@gmail.com>
2025-01-01 19:32:27 -0500

    Flatten: remove old file Sources/Test3/Test3.swiftpm/ContentView.swift

--- Sources/Test3/Test3.swiftpm/ContentView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-public struct ContentView: View {
-    public init() {}  // public initializer
-    public var body: some View {
-        Text("Hello from My Apps’s ContentView!")
-    }
-}
